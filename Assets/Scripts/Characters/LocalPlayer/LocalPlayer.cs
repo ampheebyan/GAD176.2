cb6416486f8f54fd41a53cfeea75d401981423b9
phoebe <1033478@student.sae.edu.au>
2024-11-21 11:21:25 +1100

    Implementation of GlobalReference. Beginning work on Detection stuff. Updates to LocalPlayer making use of GlobalReference to have a consistently referenceable object for LocalPlayer.

--- Assets/Scripts/Characters/LocalPlayer/LocalPlayer.cs
+++ Assets/Scripts/Characters/LocalPlayer/LocalPlayer.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 // Phoebe Faith (1033478)
 
 /// <summary>
@@ -7,4 +8,22 @@
 /// </summary>
 public class LocalPlayer : BasePlayer
 {
+    private void OnEnable()
+    {
+        if (TryGetComponent<MovementHandler>(out MovementHandler movementHandler))
+        {
+            // Failsafe in the event that localPlayer is put on something with no locally controlled movement.
+            if(GlobalReference.localPlayer == null) GlobalReference.localPlayer = this; 
+        }
+        else
+        {
+            this.enabled = false;
+            throw new Exception("LocalPlayer is on something without locally controlled movement. Disabling LocalPlayer. It should only be in a scene once, and only for the locally controlling player.");
+        }
+    }
+
+    private void OnDisable()
+    {
+        if(GlobalReference.localPlayer == this) GlobalReference.localPlayer = null; // Unassign it but only if it's this.
+    }
 }
